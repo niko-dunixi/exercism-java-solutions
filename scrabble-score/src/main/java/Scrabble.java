ff0eea7e356c7a7708a81b668b7759be61dec89a
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-28 17:50:28 -0600

    Scores scrabble string
    Quick'n dirty implementation.

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,34 @@
 class Scrabble {
 
+    private final int finalScore;
+
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        char[] letters = word.toLowerCase().toCharArray();
+
+        int score = 0;
+        for (char letter : letters) {
+            if ("aeioulnrst".indexOf(letter) >= 0) {
+                score += 1;
+            } else if ("dg".indexOf(letter) >= 0) {
+                score += 2;
+            } else if ("bcmp".indexOf(letter) >= 0) {
+                score += 3;
+            } else if ("fhvwy".indexOf(letter) >= 0) {
+                score += 4;
+            } else if ('k' == letter) {
+                score += 5;
+            } else if ("jx".indexOf(letter) >= 0) {
+                score += 8;
+            } else if ("qz".indexOf(letter) >= 0) {
+                score += 10;
+            }
+        }
+
+        finalScore = score;
     }
 
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return finalScore;
     }
 
 }
