6e84864d8fc15d4e84c6a551f8dc3e83b1c2b11e
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-28 18:08:52 -0600

    Simple implementation
    quick'n dirty implementation.

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,20 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String result = "";
+        if (number % 3 == 0) {
+            result += "Pling";
+        }
+        if (number % 5 == 0) {
+            result += "Plang";
+        }
+        if (number % 7 == 0) {
+            result += "Plong";
+        }
+        if (result.isEmpty()){
+            result += number;
+        }
+        return result;
     }
 
 }
