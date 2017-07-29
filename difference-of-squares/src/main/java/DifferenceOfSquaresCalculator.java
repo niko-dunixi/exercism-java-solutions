ad79ea2a24af99bddf0513ec7f02764950580741
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-28 18:36:45 -0600

    computeDifferenceOfSquares tests passing
    Given a number, we take computeSquareOfSumTo and then subtract
    computeSumOfSquaresTo from that value.

--- difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -18,6 +18,6 @@ public class DifferenceOfSquaresCalculator {
     }
 
     public int computeDifferenceOfSquares(int number) {
-        throw new UnsupportedOperationException("Not yet implemented."); // FIXME: via TDD
+        return computeSquareOfSumTo(number) - computeSumOfSquaresTo(number);
     }
 }
