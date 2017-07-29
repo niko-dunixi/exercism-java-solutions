1eef6bbabef26b5751bc8def293b2a7d48a9d375
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-28 18:33:41 -0600

    Passing computSquareOfSumTo tests
    You add all the numbers up to and including a limit and then square that
    summed value.

--- difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -2,7 +2,11 @@ public class DifferenceOfSquaresCalculator {
 
 
     public int computeSquareOfSumTo(int number) {
-        throw new UnsupportedOperationException("Not yet implemented."); // FIXME: via TDD
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+        }
+        return (int) Math.pow(sum, 2);
     }
 
     public int computeSumOfSquaresTo(int number) {
