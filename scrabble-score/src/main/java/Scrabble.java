f797d766b29343c0f6566d3a839173782a68bf25
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-28 18:02:43 -0600

    Slightly more complicated solution

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,4 +1,38 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 class Scrabble {
+
+    private static final Map<Character, Integer> lettersToPoints;
+
+    static {
+        HashMap<Character, Integer> characterIntegerHashMap = new HashMap<>();
+        for (char letter : new char[]{'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'}) {
+            characterIntegerHashMap.put(letter, 1);
+        }
+        for (char letter : new char[]{'d', 'g'}) {
+            characterIntegerHashMap.put(letter, 2);
+        }
+        for (char letter : new char[]{'b', 'c', 'm', 'p'}) {
+            characterIntegerHashMap.put(letter, 3);
+        }
+        for (char letter : new char[]{'f', 'h', 'v', 'w', 'y'}) {
+            characterIntegerHashMap.put(letter, 4);
+        }
+        for (char letter : new char[]{'k'}) {
+            characterIntegerHashMap.put(letter, 5);
+        }
+        for (char letter : new char[]{'j', 'x'}) {
+            characterIntegerHashMap.put(letter, 8);
+        }
+        for (char letter : new char[]{'q', 'z'}) {
+            characterIntegerHashMap.put(letter, 10);
+        }
+
+        lettersToPoints = Collections.unmodifiableMap(characterIntegerHashMap);
+    }
+
 
     private final int finalScore;
 
@@ -7,20 +41,8 @@ class Scrabble {
 
         int score = 0;
         for (char letter : letters) {
-            if ("aeioulnrst".indexOf(letter) >= 0) {
-                score += 1;
-            } else if ("dg".indexOf(letter) >= 0) {
-                score += 2;
-            } else if ("bcmp".indexOf(letter) >= 0) {
-                score += 3;
-            } else if ("fhvwy".indexOf(letter) >= 0) {
-                score += 4;
-            } else if ('k' == letter) {
-                score += 5;
-            } else if ("jx".indexOf(letter) >= 0) {
-                score += 8;
-            } else if ("qz".indexOf(letter) >= 0) {
-                score += 10;
+            if (lettersToPoints.containsKey(letter)) {
+                score += lettersToPoints.get(letter);
             }
         }
 
