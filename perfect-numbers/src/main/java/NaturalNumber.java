f56664b2def2ced32c5dd7c13539978f5f419061
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-29 00:16:36 -0600

    Adding implementation.

--- perfect-numbers/src/main/java/NaturalNumber.java
+++ perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,11 +1,30 @@
 public class NaturalNumber {
 
+    private Classification classification;
+
     public NaturalNumber(int number) {
-        throw new UnsupportedOperationException("TODO: TDD");
+        if (number <= 0) {
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+        }
+
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum < number) {
+            classification = Classification.DEFICIENT;
+        } else if (sum > number) {
+            classification = Classification.ABUNDANT;
+        } else {
+            classification = Classification.PERFECT;
+        }
     }
-    
+
     public Classification getClassification() {
-        throw new UnsupportedOperationException("TODO: TDD");
+        return classification;
     }
 
 }
