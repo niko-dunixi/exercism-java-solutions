d566710635ea50c11c31854fce43abd937e9264b
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-28 18:28:55 -0600

    Adding file to satisfy compiler, but throwing exception to satisfy tdd

--- difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -4,4 +4,12 @@ public class DifferenceOfSquaresCalculator {
     public int computeSquareOfSumTo(int number) {
         throw new UnsupportedOperationException("Not yet implemented."); // FIXME: via TDD
     }
+
+    public int computeSumOfSquaresTo(int number) {
+        throw new UnsupportedOperationException("Not yet implemented."); // FIXME: via TDD
+    }
+
+    public int computeDifferenceOfSquares(int number) {
+        throw new UnsupportedOperationException("Not yet implemented."); // FIXME: via TDD
+    }
 }
