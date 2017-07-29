8b8d70d373cf6164b0ff9cf63a1e4de9c5998f29
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-28 23:54:33 -0600

    Adding implementation for secret-handshake. Passes all tests.

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,32 @@
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        // ArrayLists can have an initial capacity set, however, it is 10 at minimum (and we'll never have that many)
+        List<Signal> result = new LinkedList<>();
+        if (isBitSet(number, 0)) {
+            result.add(Signal.WINK);
+        }
+        if (isBitSet(number, 1)) {
+            result.add(Signal.DOUBLE_BLINK);
+        }
+        if (isBitSet(number, 2)) {
+            result.add(Signal.CLOSE_YOUR_EYES);
+        }
+        if (isBitSet(number, 3)) {
+            result.add(Signal.JUMP);
+        }
+        if (isBitSet(number, 4)) {
+            Collections.reverse(result);
+        }
+        return result;
+    }
+
+    private boolean isBitSet(int number, int bitPosition) {
+        return ((number >>> bitPosition) & 0b1) == 1;
     }
 
 }
