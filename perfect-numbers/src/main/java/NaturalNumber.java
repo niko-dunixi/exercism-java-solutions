702577bd2219406446841bc2de82832de24c76c5
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-29 00:09:02 -0600

    Satisfy compiler

--- perfect-numbers/src/main/java/NaturalNumber.java
+++ perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,5 +1,11 @@
-class NaturalNumber {
+public class NaturalNumber {
 
-
+    public NaturalNumber(int number) {
+        throw new UnsupportedOperationException("TODO: TDD");
+    }
+    
+    public Classification getClassification() {
+        throw new UnsupportedOperationException("TODO: TDD");
+    }
 
 }
