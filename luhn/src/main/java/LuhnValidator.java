6b8bf3462e91ef23c82b0a18f191d8b69fd5d8bf
Paul Baker <paul.nelson.baker@gmail.com>
2017-08-11 14:01:26 -0600

    Passes all tests

--- luhn/src/main/java/LuhnValidator.java
+++ luhn/src/main/java/LuhnValidator.java
@@ -1,7 +1,23 @@
+import java.util.Arrays;
+
 class LuhnValidator {
 
     boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        // Scrub string
+        candidate = candidate.replaceAll("\\s", "");
+        // Toss out invalid strings
+        if (candidate.length() <= 1 || candidate.matches(".*\\D.*")) {
+            return false;
+        }
+        // Convert to digits and sum
+        int[] digits = Arrays.stream(candidate.split("")).mapToInt(Integer::parseInt).toArray();
+        for (int i = digits.length - 2; i >= 0; i -= 2) {
+            int currentDigit = digits[i];
+            if (currentDigit != 9) {
+                digits[i] = (currentDigit * 2) % 9;
+            }
+        }
+        return Arrays.stream(digits).sum() % 10 == 0;
     }
 
 }
--- luhn/src/test/java/LuhnValidatorTest.java
+++ luhn/src/test/java/LuhnValidatorTest.java
@@ -1,4 +1,3 @@
-import org.junit.Ignore;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,7 +9,7 @@ public class LuhnValidatorTest {
 
     @Before
     public void setUp() {
-      luhnValidator = new LuhnValidator();
+        luhnValidator = new LuhnValidator();
     }
 
     @Test
@@ -18,73 +17,73 @@ public class LuhnValidatorTest {
         assertFalse(luhnValidator.isValid("1"));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatTheStringConsistingOfASingleZeroIsInvalid() {
         assertFalse(luhnValidator.isValid("0"));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatASimpleValidNumberIsIdentifiedAsValid() {
         assertTrue(luhnValidator.isValid(" 5 9 "));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatAValidCanadianSocialInsuranceNumberIsIdentifiedAsValidV1() {
         assertTrue(luhnValidator.isValid("046 454 286"));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatAValidCanadianSocialInsuranceNumberIsIdentifiedAsValidV2() {
         assertTrue(luhnValidator.isValid("055 444 285"));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatAnInvalidCanadianSocialInsuranceNumberIsIdentifiedAsInvalid() {
         assertFalse(luhnValidator.isValid("046 454 287"));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatAnInvalidCreditCardIsIdentifiedAsInvalid() {
         assertFalse(luhnValidator.isValid("8273 1232 7352 0569"));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatAddingANonDigitCharacterToAValidStringInvalidatesTheString() {
         assertFalse(luhnValidator.isValid("046a 454 286"));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatStringContainingPunctuationIsInvalid() {
         assertFalse(luhnValidator.isValid("055-444-285"));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatStringContainingSymbolsIsInvalid() {
         assertFalse(luhnValidator.isValid("055£ 444$ 285"));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatTheStringConsistingOfASpaceAndASingleZeroIsInvalid() {
         assertFalse(luhnValidator.isValid(" 0"));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatStringContainingMultipleZerosIsValid() {
         assertTrue(luhnValidator.isValid(" 00000"));
     }
 
-    @Ignore("Remove to run test")
+    //    @Ignore("Remove to run test")
     @Test
     public void testThatDoublingNineIsHandledCorrectly() {
         assertTrue(luhnValidator.isValid("091"));
